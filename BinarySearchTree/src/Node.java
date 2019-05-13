cd5fc50afa5065c6b3764076a658a4d925e77a6f
zhaorende <897883249@qq.com>
2019-05-14 00:07:20 +0800

    二叉树-中序/前序/后序排序，查找，删除，添加

---
+++ BinarySearchTree/src/Node.java
@@ -0,0 +1,17 @@
+/**
+ * Created by 赵人德 on 2019-05-13.
+ */
+public class Node {
+    int data;   //节点数据
+    Node leftChild; //左子节点的引用
+    Node rightChild; //右子节点的引用
+    boolean isDelete;//表示节点是否被删除
+
+    public Node(int data){
+        this.data = data;
+    }
+    //打印节点内容
+    public void display(){
+        System.out.println(data);
+    }
+}
